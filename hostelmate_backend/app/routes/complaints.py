f91b6081323bc86ddd8d41d4872236a71133ae55
Himani Bisht <himanibisht515@gmail.com>
2026-04-21 22:50:25 +0530

    complaint feature  done

--- hostelmate_backend/app/routes/complaints.py
+++ hostelmate_backend/app/routes/complaints.py
@@ -1,5 +1,6 @@
 from flask import Blueprint, request, jsonify
 from app.extensions import db
+from flask_jwt_extended import jwt_required, get_jwt_identity
 from app.models.complaints import Complaint
 from app.models.hostel import Hostels
 from app.models.user import User
@@ -7,33 +8,29 @@ from datetime import datetime
 
 complaint_bp = Blueprint("complaint_bp", __name__)
 
+
 @complaint_bp.route("/api/complaints", methods=["POST"])
+@jwt_required()
 def create_complaint():
     data = request.get_json()
 
-   
-    if not data.get("category") or not data.get("priority") or not data.get("description"):
-        return jsonify({"error": "Missing required fields"}), 400
+    required_fields = ["hostel_id", "category", "priority", "description"]
+    for field in required_fields:
+        if not data.get(field):
+            return jsonify({"error": f"{field} is required"}), 400
 
-    
+    current_user_id = get_jwt_identity()
     is_anonymous = data.get("is_anonymous", False)
-
-    
-    current_user_id = 1   
-
-    if is_anonymous:
-        user_id = None   
-    else:
-        user_id = current_user_id
+    user_id = None if is_anonymous else current_user_id
 
     complaint = Complaint(
         user_id=user_id,
-        hostel_id=data.get("hostel_id"),
+        hostel_id=data["hostel_id"],
         category=data["category"],
         priority=data["priority"],
         description=data["description"],
         is_anonymous=is_anonymous,
-        status="Pending",
+        status="pending",
         created_at=datetime.utcnow()
     )
 
